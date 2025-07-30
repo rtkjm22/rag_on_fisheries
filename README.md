bcd2d0d6dcb4b6c563248010713d691596519e81
Ryota Kojima <ryotakojima@RyotanoMacBook-Pro.local>
2025-07-31 01:55:02 +0900

    feat:readme更新

---
+++ README.md
@@ -0,0 +1,6 @@
+## DB 立ち上げ
+
+```bash
+brew services start postgresql@14
+brew services stop postgresql@14
+```
--- app/models/embedding_model.py
+++ app/models/embedding_model.py
@@ -3,9 +3,7 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 
 
 def get_embedding_function():
-    return HuggingFaceEmbeddings(
-        model_name="intfloat/multilingual-e5-small"
-    )
+    return HuggingFaceEmbeddings(model_name="intfloat/multilingual-e5-small")
 
 
 # model = SentenceTransformer("cl-tohoku/bert-base-japanese-whole-word-masking")
